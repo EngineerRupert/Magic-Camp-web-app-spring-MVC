ef578a6fc7e085764cfadc837c87a45c982d6533
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2021-10-16 19:28:55 +0300

    Added getter and setter to User

--- src/main/java/ru/magiccamp/project/model/User.java
+++ src/main/java/ru/magiccamp/project/model/User.java
@@ -16,12 +16,69 @@ public class User {
     @Column(nullable = false, length = 150)
     private String password;
 
+    @Column(length = 100)
+    private String dataOfBirth;
+
+    @Column(length = 150)
+    private String zodiacSign;
+
+    @Column(length = 150)
+    private String yearOfBirthSign;
+
     public User() {
 
     }
 
     public User(String login, String password) {
         this.login = login;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getDataOfBirth() {
+        return dataOfBirth;
+    }
+
+    public void setDataOfBirth(String dataOfBirth) {
+        this.dataOfBirth = dataOfBirth;
+    }
+
+    public String getZodiacSign() {
+        return zodiacSign;
+    }
+
+    public void setZodiacSign(String zodiacSign) {
+        this.zodiacSign = zodiacSign;
+    }
+
+    public String getYearOfBirthSign() {
+        return yearOfBirthSign;
+    }
+
+    public void setYearOfBirthSign(String yearOfBirthSign) {
+        this.yearOfBirthSign = yearOfBirthSign;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
