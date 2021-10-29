a6df78a2ff16afbe96daabc697f8966e5dc6d2b7
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2021-10-29 18:48:11 +0300

    Added ProdJpaConfig

---
+++ src/main/java/ru/magiccamp/project/config/ProdJpaConfig.java
@@ -0,0 +1,17 @@
+package ru.magiccamp.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@Configuration
+public class ProdJpaConfig {
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        return Persistence.createEntityManagerFactory("TestPersistenceUnit");
+    }
+
+}
