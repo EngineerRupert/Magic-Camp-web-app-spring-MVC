869cc7aea05ba6a031acb0f39649dbca4980fb99
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2021-10-31 23:41:55 +0300

    Added SecurityConfig

---
+++ src/main/java/ru/magiccamp/project/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package ru.magiccamp.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf();
+
+        http.authorizeRequests()
+                .antMatchers("/", "/user/log-in", "/user/register", "/log-in").permitAll()
+                .antMatchers("/**/*.css").permitAll()
+                .antMatchers(HttpMethod.GET).permitAll()
+                .antMatchers( "/user/**").authenticated()
+                .antMatchers("/api/*").authenticated()
+                .anyRequest().denyAll();
+
+        http.formLogin()
+                .loginPage("/user/log-in")
+                .loginProcessingUrl("/user/log-in")
+                .usernameParameter("login")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .permitAll();
+
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
