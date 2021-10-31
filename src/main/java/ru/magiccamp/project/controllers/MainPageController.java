960467f00028d22f60547dd5d06cc28d389d6fdd
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2021-10-31 23:39:18 +0300

    Added MainPageController

---
+++ src/main/java/ru/magiccamp/project/controllers/MainPageController.java
@@ -0,0 +1,26 @@
+package ru.magiccamp.project.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/")
+public class MainPageController {
+
+    @GetMapping("/")
+    public String index(Model model,
+                        Authentication authentication) {
+        boolean loggedIn = authentication != null && authentication.isAuthenticated();
+        String userName = "";
+        if (loggedIn) {
+            userName = authentication.getName();
+        }
+        model.addAttribute("loggedIn", loggedIn);
+        model.addAttribute("getLogin", userName);
+        return "index";
+    }
+
+}
