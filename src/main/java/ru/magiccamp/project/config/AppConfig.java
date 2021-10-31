98fb86001d2784629bd4dc2a6be73f2e153934ab
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2021-10-31 23:40:45 +0300

    Added AppConfig

--- src/main/java/ru/magiccamp/project/config/AppConfig.java
+++ src/main/java/ru/magiccamp/project/config/AppConfig.java
@@ -1,13 +1,24 @@
 package ru.magiccamp.project.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @Configuration
 @ComponentScan(basePackages = "ru.magiccamp.project")
-@EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "ru.magiccamp.project.dao")
 public class AppConfig {
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        return Persistence.createEntityManagerFactory("TestPersistenceUnit");
+    }
+
+    @Bean
+    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
+    }
+
 }
