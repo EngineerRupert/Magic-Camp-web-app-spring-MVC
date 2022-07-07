c91cfbb9c2e39de44c8a448b7522e46e7040f244
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2022-07-07 18:26:33 +0300

    Refactoring of NameDecryption

--- src/main/java/ru/magiccamp/project/service/NameDecryption.java
+++ src/main/java/ru/magiccamp/project/service/NameDecryption.java
@@ -16,51 +16,39 @@ public class NameDecryption {
         var mapOfCharsAndDigits = mapOfChars();
         var sum = List.of(name.trim().toLowerCase().split(""))
                 .stream()
-                .map(o -> mapOfCharsAndDigits.get(o))
+                .map(mapOfCharsAndDigits::get)
                 .mapToInt(e -> e)
                 .sum();
         return List.of(String.valueOf(sum).trim().split(""))
                 .stream()
-                .mapToInt(e -> Integer.parseInt(e))
+                .mapToInt(Integer::parseInt)
                 .sum();
     }
 
     private Map<String, Integer> mapOfChars() {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("а", 1);
-        map.put("б", 2);
-        map.put("в", 3);
-        map.put("г", 4);
-        map.put("д", 5);
-        map.put("е", 6);
-        map.put("ё", 7);
-        map.put("ж", 8);
-        map.put("з", 9);
-        map.put("и", 1);
-        map.put("й", 2);
-        map.put("к", 3);
-        map.put("л", 4);
-        map.put("м", 5);
-        map.put("н", 6);
-        map.put("о", 7);
-        map.put("п", 8);
-        map.put("р", 9);
-        map.put("с", 1);
-        map.put("т", 2);
-        map.put("у", 3);
-        map.put("ф", 4);
-        map.put("х", 5);
-        map.put("ц", 6);
-        map.put("ч", 7);
-        map.put("ш", 8);
-        map.put("щ", 9);
-        map.put("ъ", 1);
-        map.put("ы", 2);
-        map.put("ь", 3);
-        map.put("э", 4);
-        map.put("ю", 5);
-        map.put("я", 6);
-        return map;
+        Map<String, Integer> mapResultNumbers = new HashMap<>();
+        mapResultNumbers.putAll(Map.of(
+                "а", 1,"б",2,"в",3,
+                "г", 4,"д",5,"е",6,
+                "ё", 7,"ж", 8,"з",9,
+                "и", 1
+        ));
+        mapResultNumbers.putAll(Map.of(
+                "й", 2,"к",3,"л",4,
+                "м", 5,"н",6,"о",7,
+                "п", 8,"р",9,"с",1,
+                "т", 2
+        ));
+        mapResultNumbers.putAll(Map.of(
+                "у", 3,"ф",4,"х",5,
+                "ц", 6,"ч",7,"ш",8,
+                "щ", 9,"ъ",1,"ы",2,
+                "ь", 3
+        ));
+        mapResultNumbers.putAll(Map.of(
+                "э", 4,"ю", 5,"я", 6
+        ));
+        return mapResultNumbers;
     }
 
 }
